51c5fbe0b776fc2ec28298f137c3495d733f750a
Nattanan <nattanan.pim@ku.th>
2024-12-02 00:00:49 +0700

    make the control more oo style

--- Player.py
+++ Player.py
@@ -15,6 +15,7 @@ class Player:
         self.body.speed(self.speed)
         self.body.setheading(90)
         self.keys = {"w": False, "s": False, "a": False, "d": False}
+        self.screen = turtle.Screen()
 
     def movement(self):
         if self.keys["w"]:
@@ -33,23 +34,26 @@ class Player:
     def dash(self, status):
         self.speed = 2 + (status * 10)
 
+    def controlled(self):
+        self.screen.onkeypress(lambda: self.c_keys("w", True), "w")
+        self.screen.onkeyrelease(lambda: self.c_keys("w", False), "w")
+        self.screen.onkeypress(lambda: self.c_keys("s", True), "s")
+        self.screen.onkeyrelease(lambda: self.c_keys("s", False), "s")
+        self.screen.onkeypress(lambda: self.c_keys("a", True), "a")
+        self.screen.onkeyrelease(lambda: self.c_keys("a", False), "a")
+        self.screen.onkeypress(lambda: self.c_keys("d", True), "d")
+        self.screen.onkeyrelease(lambda: self.c_keys("d", False), "d")
+        # hope user don't hold space plssss just don't I don't know how to make them properly this shit is just a
+        # witchcraft. this shouldn't be for speeding
+        self.screen.onkeypress(lambda: self.dash(True), "space")
+        self.screen.onkeyrelease(lambda: self.dash(False), "space")
 
+
+# for run part
 player = Player()
-wn = turtle.Screen()
-wn.onkeypress(lambda: player.c_keys("w", True), "w")
-wn.onkeyrelease(lambda: player.c_keys("w", False), "w")
-wn.onkeypress(lambda: player.c_keys("s", True), "s")
-wn.onkeyrelease(lambda: player.c_keys("s", False), "s")
-wn.onkeypress(lambda: player.c_keys("a", True), "a")
-wn.onkeyrelease(lambda: player.c_keys("a", False), "a")
-wn.onkeypress(lambda: player.c_keys("d", True), "d")
-wn.onkeyrelease(lambda: player.c_keys("d", False), "d")
-# hope user don't hold space plssss just don't I don't know how to make them properly this shit is just a witchcraft
-# this shouldn't be for speeding
-wn.onkeypress(lambda: player.dash(True), "space")
-wn.onkeyrelease(lambda: player.dash(False), "space")
 
-wn.listen()
+player.controlled()
+player.screen.listen()
 
 while True:
     player.movement()
