73c229a7e54ea3e1520a4eea6df12120b84c2374
Nattanan <nattanan.pim@ku.th>
2024-12-05 16:24:07 +0700

    make the player can get dots and make dots disapear and reappear

--- Game.py
+++ Game.py
@@ -1,14 +1,16 @@
 import turtle
 import Ball
-import math
 import Player
 import random
+import heapq
+import my_event
+import dot
 
 
 class RunGame:
-    def __init__(self, balls=5, dots=5):
+    def __init__(self, balls=5, dots=10):
         self.num_balls = balls
-        self.balls_lst = []
+        self.ball_list = []
         self.num_dots = dots
         self.dos_lst = []
         self.t = 0.0
@@ -22,14 +24,18 @@ class RunGame:
         self.canvas_height = turtle.screensize()[1]
         self.player = Player.Player(self.canvas_width, self.canvas_height)
         print(self.canvas_width, self.canvas_height)
-        self.ball_rad = int(0.05 * self.canvas_height)
+        self.ball_rad = int(0.1 * self.canvas_height)
         for i in range(self.num_balls):
             ball = Ball.Ball(self.ball_rad,
                              random.randint(-self.canvas_width + self.ball_rad, self.canvas_width - self.ball_rad),
                              random.randint(-self.canvas_height + self.ball_rad, self.canvas_height - self.ball_rad),
-                             1, 1, 1,
+                             random.randint(-10, 10), random.randint(-10, 10), i,
                              self.canvas_width, self.canvas_height)
-            self.balls_lst.append(ball)
+            self.ball_list.append(ball)
+        for i in range(self.num_dots):
+            d = dot.Dot(random.randint(-self.canvas_width + 20, self.canvas_width - 20),
+                        random.randint(-self.canvas_height + 20, self.canvas_height - 20))
+            self.dos_lst.append(d)
 
     def __draw_border(self):
         turtle.hideturtle()
@@ -44,10 +50,18 @@ class RunGame:
             turtle.forward(2 * self.canvas_height)
             turtle.left(90)
 
+    def __redraw(self):
+        self.__draw_border()
+        for d in self.dos_lst:
+            d.draw()
+        for ball in self.ball_list:
+            ball.draw()
+
     def run(self):
+
         self.__draw_border()
         turtle.hideturtle()
-        for ball in self.balls_lst:
+        for ball in self.ball_list:
             ball.draw()
 
         self.player.controlled()
@@ -57,12 +71,18 @@ class RunGame:
             self.player.check_wall()
             self.player.body.showturtle()
             self.player.movement()
-            for ball in self.balls_lst:
+            for ball in self.ball_list:
                 ball.move(0.01)
+            for d in self.dos_lst:
+                if self.player.distance(d) <= (d.radius + 10):
+                    self.player.score += 1
+                    self.dos_lst.remove(d)
+            if len(self.dos_lst) <= 8:
+                d = dot.Dot(random.randint(-self.canvas_width + 20, self.canvas_width - 20),
+                            random.randint(-self.canvas_height + 20, self.canvas_height - 20))
+                self.dos_lst.append(d)
             turtle.clear()
-            self.__draw_border()
-            for ball in self.balls_lst:
-                ball.draw()
+            self.__redraw()
 
 
 run = RunGame()
